bc81a1e5b0ca80eb9c103c7f88f1fa2b63d9e295
Will Daly <will@edx.org>
2015-06-25 14:49:31 -0700

    Credit eligibility/provider refactor
    * Remove m2m relation between credit course and credit providers.
    * Separate eligibility and provider APIs into different modules.
    * Add API call for retrieving a user's eligibilities.
    * Cache credit course list.
    * Style the dashboard purchase button.
    * Display a link for the credit provider on the dashboard.
    * Add analytics events for clicks on the purchase button.
    * Expose more credit models to Django admin and add search functionality.

--- openedx/core/djangoapps/credit/signals.py
+++ openedx/core/djangoapps/credit/signals.py
@@ -9,6 +9,8 @@ from opaque_keys.edx.keys import CourseKey
 from xmodule.modulestore.django import SignalHandler
 from openedx.core.djangoapps.signals.signals import GRADES_UPDATED
 
+from openedx.core.djangoapps.credit import api
+
 
 @receiver(SignalHandler.course_published)
 def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=unused-argument
@@ -39,24 +41,20 @@ def listen_for_grade_calculation(sender, username, grade_summary, course_key, de
         kwargs : None
 
     """
-    from openedx.core.djangoapps.credit.api import (
-        is_credit_course, get_credit_requirement, set_credit_requirement_status
-    )
-
     course_id = CourseKey.from_string(unicode(course_key))
-    is_credit = is_credit_course(course_id)
+    is_credit = api.is_credit_course(course_id)
     if is_credit:
-        requirement = get_credit_requirement(course_id, 'grade', 'grade')
-        if requirement:
-            criteria = requirement.get('criteria')
+        requirements = api.get_credit_requirements(course_id, namespace='grade')
+        if requirements:
+            criteria = requirements[0].get('criteria')
             if criteria:
                 min_grade = criteria.get('min_grade')
                 if grade_summary['percent'] >= min_grade:
                     reason_dict = {'final_grade': grade_summary['percent']}
-                    set_credit_requirement_status(
+                    api.set_credit_requirement_status(
                         username, course_id, 'grade', 'grade', status="satisfied", reason=reason_dict
                     )
                 elif deadline and deadline < timezone.now():
-                    set_credit_requirement_status(
+                    api.set_credit_requirement_status(
                         username, course_id, 'grade', 'grade', status="failed", reason={}
                     )
