e036c5bd9e304c7b79f454ca5c63a26c513017cf
Christine Lytwynec <clytwynec@edx.org>
2015-07-01 10:57:29 -0400

    add coveralls

--- scripts/jenkins-report.sh
+++ scripts/jenkins-report.sh
@@ -2,7 +2,11 @@
 source scripts/jenkins-common.sh
 
 # Get the diff coverage and html reports for unit tests
-NO_PREREQ_INSTALL=true paver coverage
+paver coverage
+
+# send the coverage data to coveralls
+pip install coveralls
+COVERALLS_REPO_TOKEN=$1 coveralls
 
 # Get coverage reports for bok choy
 # paver bokchoy_coverage
